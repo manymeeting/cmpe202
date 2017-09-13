813e4bd7c113787490df97030340497b5d32b1d6
Mokuten OH <mokuteno@gmail.com>
2017-09-12 18:29:54 -0700

    modify Main function to run the application

--- gumball/gumball-java-pattern/Main.java
+++ gumball/gumball-java-pattern/Main.java
@@ -1,22 +1,60 @@
-
+package gumball.pattern;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
 
-		System.out.println(gumballMachine);
+		// A quarter-only machine with 25 cents cost
+		GumballMachine gumballMachineQ25 = new GumballMachine(5, 25, true);
 
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
+		System.out.println(gumballMachineQ25);
 
-		System.out.println(gumballMachine);
+		gumballMachineQ25.insertCoins(25);
+		gumballMachineQ25.turnCrank();
 
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
+		System.out.println(gumballMachineQ25);
 
-		System.out.println(gumballMachine);
+		gumballMachineQ25.insertCoins(25);
+		gumballMachineQ25.turnCrank();
+		gumballMachineQ25.insertCoins(10);
+		gumballMachineQ25.turnCrank();
+
+		System.out.println(gumballMachineQ25);
+
+		// An quarter-only machine with 50 cents cost
+		GumballMachine gumballMachineQ50 = new GumballMachine(5, 50, true);
+
+		System.out.println(gumballMachineQ50);
+
+		gumballMachineQ50.insertCoins(25);
+		gumballMachineQ50.turnCrank();
+
+		System.out.println(gumballMachineQ50);
+
+		gumballMachineQ50.insertCoins(50);
+		gumballMachineQ50.insertCoins(25);
+		gumballMachineQ50.turnCrank();
+
+		System.out.println(gumballMachineQ50);
+
+		// An all-coins machine with 50 cents cost
+		GumballMachine gumballMachineA50 = new GumballMachine(5, 50, false);
+
+		System.out.println(gumballMachineA50);
+
+		gumballMachineA50.insertCoins(10);
+		gumballMachineA50.turnCrank();
+
+		System.out.println(gumballMachineA50);
+
+		gumballMachineA50.insertCoins(10);
+		gumballMachineA50.turnCrank();
+		gumballMachineA50.insertCoins(5);
+		gumballMachineA50.turnCrank();
+		gumballMachineA50.insertCoins(25);
+		gumballMachineA50.turnCrank();
+
+		System.out.println(gumballMachineA50);
+
 	}
 }
